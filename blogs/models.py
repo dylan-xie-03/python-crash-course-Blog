38ba2f15ca0bb7f61a738f5a646f75d7223f227c
xie <xie@gmail.com>
2021-01-04 07:31:52 +0800

    success

--- blogs/models.py
+++ blogs/models.py
@@ -1,14 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
     """A Blog user post"""
     title = models.CharField(max_length=200)
     text = models.TextField(max_length=400)
     date_added = models.DateTimeField(auto_now_add=True)
-
-    # def __str__(self):
-    #     """Return a string represention of the model."""
-    #     return f"{self.title}  --  {self.text}"
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """"""
--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import TemplateView
+
 app_name = 'blogs'
 urlpatterns = [
     # Home page
@@ -9,5 +11,8 @@ urlpatterns = [
     # Page for adding a new blog
     path('new_blog/', views.new_blog, name='new_blog'),
     # Page for editting an entry.
-    path('edit_blogpost/<int:blog_id>/', views.edit_blogpost, name='edit_blogpost')
+    path('edit_blogpost/<int:blog_id>/', views.edit_blogpost, name='edit_blogpost'),
+    # not allow
+    # path('blogs/not_allow/', views.index, name='not_allow')
+    path('not_allow', TemplateView.as_view(template_name='not_allow.html'), name='not_allow'),
 ]
